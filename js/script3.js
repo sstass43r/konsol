dc6822e620a8eafa7213ce3ffa3d59291e559dcf
StWork <sstass43r@mail.ru>
2023-10-13 21:02:59 +0300

    js3 log operator end

---
+++ js/script3.js
@@ -0,0 +1,12 @@
+// логические операторы
+"use strict";
+
+
+const hamburger = 2;
+const fries = 1;
+
+if (hamburger === 3 && fries) {
+    console.log('Все сыты!')
+} else {
+    console.log('Мы уходим')
+}
