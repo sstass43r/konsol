58dc47fd73704a45c0d408bd5c12b74f81031b86
StWork <sstass43r@mail.ru>
2023-10-18 20:41:50 +0300

    edit js4

--- js/script4.js
+++ js/script4.js
@@ -1,17 +1,17 @@
-"use strict";
+'use strict';
 
 let num = 50;
 
 for (let i = 1; i < 8; i++) {
-    console.log(num);
-    num++;
+	console.log(num);
+	num++;
 }
 
 // 
 for (let i = 1; i < 10; i++) {
-    if (i === 6) {
-        // break;
-        continue;
-    }
-    console.log(i);
+	if (i === 6) {
+		// break;
+		continue;
+	}
+	console.log(i);
 }
